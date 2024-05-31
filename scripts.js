adb6ab2017aeb59b8bfdf78b04927c2a4aa0551a
Kxddeenn <kaden.neild@gmail.com>
2024-05-31 09:53:49 -0400

    getHumanChoice() function

--- scripts.js
+++ scripts.js
@@ -18,4 +18,26 @@ function getComputerChoice(){
     }
 
     return randomNumber;
+};
+
+console.log(getComputerChoice());
+
+// New function called getHumanChoice
+// Returns valid values (One of the options)
+
+function getHumanChoice(){
+
+    humanChoice = prompt("Please choose Papper, Scissors or Rock");
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice !== "papper" && humanChoice !== "rock" && humanChoice !== "papper"){
+    
+        humanChoice = prompt("Please choose Papper, Scissors or Rock");
+        
+    }
+
+    return humanChoice;
+
 }
+
+console.log(getHumanChoice());
