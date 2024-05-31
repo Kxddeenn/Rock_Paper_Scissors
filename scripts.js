40aa961d2c25e5173f97ae4b6733fe2ef7350989
Kxddeenn <kaden.neild@gmail.com>
2024-05-31 09:38:31 -0400

    getComputerChoice Function

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
-console.log("Hello World!");
 
+// Create a function that returns strings "rock", "papper", "scissors"
+// Called getComputerChoice
+
+function getComputerChoice(){
+    randomNumber = Math.floor(Math.random() * 100);
+    
+    if (randomNumber < 33.3){
+        randomNumber = "Rock";
+    }
+
+    else if (randomNumber >= 33.3 && randomNumber < 66.6){
+        randomNumber = "Scissors";
+    }
+    
+    else {
+        randomNumber = "Papper";
+    }
+
+    return randomNumber;
+}
